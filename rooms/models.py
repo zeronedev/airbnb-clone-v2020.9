07e3ddc5a611d0de70db63ac9457f4c4082a58cc
zeronedev <ok7375@gmail.com>
2020-09-13 18:04:33 +0900

    #4.3 ManyToMany like a Boss

--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,22 @@ from core import models as core_models
 from users import models as user_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Mata:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    pass
+
+
 class Room(core_models.TimeStampedModel):
     """ Room Model Definition """
 
@@ -21,3 +37,7 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
