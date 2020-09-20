7863cc131cb5b4046eeb20f06cee0cf4da9e4cbf
zeronedev <ok7375@gmail.com>
2020-09-20 19:29:06 +0900

    #5.3 Conversation and Messages Models

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,9 @@ PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
+    "conversations.apps.ConversationsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
