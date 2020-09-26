1dcc9e1d1bc1f90d1f0cede8de259f4c416bc01d
zeronedev <ok7375@gmail.com>
2020-09-26 15:18:19 +0900

    #7.2 Many to Many _sets

--- reservations/models.py
+++ reservations/models.py
@@ -4,7 +4,7 @@ from core import models as core_models
 
 class Reservation(core_models.TimeStampedModel):
 
-    """ Reservation Model Definition """
+    """ Reservation(예약) Model Definition """
 
     STATUS_PENDING = "pending"
     STATUS_CONFIRMED = "confirmed"
@@ -21,8 +21,12 @@ class Reservation(core_models.TimeStampedModel):
     )
     check_in = models.DateField()
     check_out = models.DateField()
-    guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+    guest = models.ForeignKey(
+        "users.User", related_name="reservations", on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reservations", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return f"{self.room} - {self.check_in} - {self.status}"
