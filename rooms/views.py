cd972f1028f39d03f12e7935689258f6b6a167ac
zeronedev <ok7375@gmail.com>
2020-10-25 07:45:24 +0900

    #10.1 HttpResponse and render

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,11 @@
+# from datetime import datetime
+# from django.http.request import HttpRequest
+# from django.http.response import HttpResponse
 from django.shortcuts import render
 
 
 def all_rooms(request):
-    pass
+    # print(dir(request))
+    # now = datetime.now()
+    # return HttpResponse(content=f"<h1>{request.user}  {now}</h1>")
+    return render(request, "all_rooms")
