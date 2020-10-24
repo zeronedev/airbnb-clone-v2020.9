fa2136ce5f7325fbbadc8f3f1728646e434d444e
zeronedev <ok7375@gmail.com>
2020-10-25 06:39:55 +0900

    #10.0 Introduction to Urls and Views

---
+++ core/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from rooms import views as room_views
+
+app_name = "core"  # config > urls.py 의 namespace와 같아야한다
+
+urlpatterns = [path("", room_views.all_rooms, name="home")]
