4a419593daec0984b92617824fb4fa4c1fae321d
zeronedev <ok7375@gmail.com>
2020-10-25 10:05:17 +0900

    #10.2 Introduction to Django Templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,8 @@
-# from datetime import datetime
-# from django.http.request import HttpRequest
-# from django.http.response import HttpResponse
+from datetime import datetime
 from django.shortcuts import render
 
 
 def all_rooms(request):
-    # print(dir(request))
-    # now = datetime.now()
-    # return HttpResponse(content=f"<h1>{request.user}  {now}</h1>")
-    return render(request, "all_rooms")
+    now = datetime.now()
+    hungry = True
+    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
