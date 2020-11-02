a9a9333426f575472a84fbc04ab21a24ecde358f
zeronedev <ok7375@gmail.com>
2020-11-03 06:26:14 +0900

    #11.7 Class Based Views

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,4 @@ from rooms import views as room_views
 
 app_name = "core"  # config > urls.py 의 namespace와 같아야한다
 
-urlpatterns = [path("", room_views.all_rooms, name="home")]
+urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,12 @@
-from django.shortcuts import redirect, render
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.views.generic.list import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)  # str
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
+class HomeView(ListView):
 
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
-    # except PageNotAnInteger:
-    #     return redirect("/")
-    # except Exception:
-    #     return redirect("/")
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
