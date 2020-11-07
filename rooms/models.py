c1aee11f3770d4eb3f8877e43668ff68c6df77e1
zeronedev <ok7375@gmail.com>
2020-11-08 07:44:38 +0900

    #12.1 get_absolute_url

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
 
@@ -88,6 +89,12 @@ class Room(core_models.TimeStampedModel):
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        print(self.pk)
+        return reverse(
+            "rooms:detail", kwargs={"pk": self.pk}
+        )  # namespace:name, pk는 rooms > urls.py 의 int:pk
 
     def total_rating(self):
         all_reviews = self.reviews.all()
